2a6d1a1e1753ec7167b9cdc8a661f769edd30b38
Markus Kasperczyk <markus-pfeifer@outlook.com>
2021-11-15 02:18:04 +0100

    repeat until good

--- Sources/Free/Utils.swift
+++ Sources/Free/Utils.swift
@@ -5,7 +5,43 @@
 //  Created by Markus Kasperczyk on 14.11.21.
 //
 
+import Foundation
 
+
+public func repeatUntilGood<Action : Symbol, Output>(maxIterations: UInt = .max,
+                                                     action: @escaping @autoclosure () -> Action)
+-> Recursive<Action> where Action.Meaning == Discriminative<Output, Void> {
+    
+    repeatUntilGood(maxIterations: maxIterations,
+                    seed: (),
+                    action: action)
+    
+}
+
+
+public func repeatUntilGood<Seed, Action : Symbol, Output>(maxIterations: UInt = .max,
+                                                           seed: Seed,
+                                                           action: @escaping (Seed) -> Action)
+-> Recursive<Action> where Action.Meaning == Discriminative<Output, Seed> {
+    
+    if maxIterations == 0 {
+        return .pure(.bad(seed))
+    }
+    
+    return .recursive(action(seed)) {result in
+        
+        switch result {
+        case .good(let good):
+            return .pure(.good(good))
+        case .bad(let newSeed):
+            return repeatUntilGood(maxIterations: maxIterations - 1,
+                                   seed: newSeed,
+                                   action: action)
+        }
+        
+    }
+    
+}
 
 
 public func repeatUntil<Cond : Symbol, Action : Symbol>(seed: Action.Meaning,
@@ -19,9 +55,10 @@ where Cond.Meaning == Discriminative<Action.Meaning, Action.Meaning> {
     
 }
 
+
 func _repeatUntil<Cond : Symbol, Action : Symbol>(seed: Action.Meaning,
-                                                 action: @escaping (Action.Meaning) -> Action,
-                                                 until condition: @escaping (Action.Meaning) -> Cond)
+                                                  action: @escaping (Action.Meaning) -> Action,
+                                                  until condition: @escaping (Action.Meaning) -> Cond)
 -> Recursive<Either<Map<Action, Cond.Meaning>, Cond>>
 where Cond.Meaning == Discriminative<Action.Meaning, Action.Meaning> {
     
@@ -44,8 +81,8 @@ where Cond.Meaning == Discriminative<Action.Meaning, Action.Meaning> {
             case .bad(let val):
                 
                 return _repeatUntil(seed: val,
-                                   action: action,
-                                   until: condition)
+                                    action: action,
+                                    until: condition)
                 
             }
             
@@ -57,21 +94,20 @@ where Cond.Meaning == Discriminative<Action.Meaning, Action.Meaning> {
 
 
 public func whileCond<Cond : Symbol, Action : Symbol>(seed: Action.Meaning,
-                                               action: @escaping (Action.Meaning) -> Action,
-                                               while condition: @escaping (Action.Meaning) -> Cond)
+                                                      action: @escaping (Action.Meaning) -> Action,
+                                                      while condition: @escaping (Action.Meaning) -> Cond)
 -> Map<Recursive<Either<Map<Action, Cond.Meaning>, Cond>>, Action.Meaning> where Cond.Meaning == Discriminative<Action.Meaning, Action.Meaning>
- {
-
-     _whileCond(seed: seed, action: action, while: condition)
-         .map{$0.get()}
-     
+{
+    
+    _whileCond(seed: seed, action: action, while: condition)
+        .map{$0.get()}
+    
 }
 
 
-
 func _whileCond<Cond : Symbol, Action : Symbol>(seed: Action.Meaning,
-                                               action: @escaping (Action.Meaning) -> Action,
-                                               while condition: @escaping (Action.Meaning) -> Cond)
+                                                action: @escaping (Action.Meaning) -> Action,
+                                                while condition: @escaping (Action.Meaning) -> Cond)
 -> Recursive<Either<Map<Action, Cond.Meaning>, Cond>>
 where Cond.Meaning == Discriminative<Action.Meaning, Action.Meaning> {
     
@@ -90,13 +126,13 @@ where Cond.Meaning == Discriminative<Action.Meaning, Action.Meaning> {
         case .bad(let val):
             
             let firstAct = CodePath.either(action(val).map(Cond.Meaning.bad))
-           
+            
             
             return .recursive(firstAct) {newValue in
                 
                 _whileCond(seed: newValue.get(),
-                          action: action,
-                          while: condition)
+                           action: action,
+                           while: condition)
                 
             }
             
